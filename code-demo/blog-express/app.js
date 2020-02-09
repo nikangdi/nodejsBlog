ff159eb7a36227243c5e2ad8662223dc36ebc0dc
Aiden <nikangdi@aliyun.com>
2020-02-09 21:18:36 +0800

    nodejsBlog_2020_02_09

---
+++ code-demo/blog-express/app.js
@@ -0,0 +1,85 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+const blogRouter = require('./routes/blog')
+const userRouter = require('./routes/user')
+
+var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+const ENV = process.env.NODE_ENV
+if (ENV !== 'production') {
+  // 开发环境 / 测试环境
+  // 直接打印到控制台上
+  app.use(logger('dev'));
+  //logger有两个参数，第二个参数默认为{stream:process.stdout}
+} else {
+  // 线上环境
+  const logFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'//流追加
+  })
+  app.use(logger('combined', {
+    stream: writeStream
+  }));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//这两个用于解析前端发过来请求中的相关信息，并挂载到req.body上
+//若前端请求content-type为application/json,传过来为json格式数据
+//app.use(express.json());会将其解析挂载
+//若前端请求content-type为application/x-www-form-urlencoded
+//则app.use(express.urlencoded({ extended: false }));进行对其解析；
+
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+const redisClient = require('./db/redis')
+const sessionStore = new RedisStore({
+  client: redisClient
+})
+app.use(session({
+  secret: 'WJiol#23123_',
+  cookie: {
+    // path: '/',   // 默认配置
+    // httpOnly: true,  // 默认配置
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: sessionStore
+}))
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use('/api/blog', blogRouter);
+app.use('/api/user', userRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
